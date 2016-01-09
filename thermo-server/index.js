c1249e70f956184e452179cd86686e26d1019f4d
Roberto Cássio Jr <roberto@escribo.com.br>
2016-01-08 21:26:40 -0300

    initial (testing) version of node.js server

---
+++ thermo-server/index.js
@@ -0,0 +1,15 @@
+var serialport = require('serialport');// include the library
+   SerialPort = serialport.SerialPort; // make a local instance of it
+   // get port name from the command line:
+   portName = process.argv[2];
+
+var myPort = new SerialPort(portName, {
+   baudRate: 9600,
+   // look for return and newline at the end of each data packet:
+   parser: serialport.parsers.readline("\n")
+ });
+
+myPort.on('open', showPortOpen);
+myPort.on('data', sendSerialData);
+myPort.on('close', showPortClose);
+myPort.on('error', showError);
