44da2b284646f9da227cbddbe59eb9d060f0a826
Roberto Cássio Jr <roberto@escribo.com.br>
2016-01-15 19:56:57 -0300

    versão inicial do servidor de exibição de temperatura

--- thermo-server/index.js
+++ thermo-server/index.js
@@ -13,10 +13,6 @@ function showPortOpen() {
    console.log('port open. Data rate: ' + myPort.options.baudRate);
 }
 
-function sendSerialData(data) {
-   console.log(data);
-}
-
 function showPortClose() {
    console.log('port closed.');
 }
@@ -29,3 +25,44 @@ myPort.on('open', showPortOpen);
 myPort.on('data', sendSerialData);
 myPort.on('close', showPortClose);
 myPort.on('error', showError);
+
+var WebSocketServer = require('ws').Server;
+var SERVER_PORT = 8081;               // port number for the webSocket server
+var wss = new WebSocketServer({port: SERVER_PORT}); // the webSocket server
+var connections = new Array;          // list of connections to the server
+
+wss.on('connection', handleConnection);
+
+function handleConnection(client) {
+ console.log("New Connection"); // you have a new client
+ connections.push(client); // add this client to the connections array
+
+ client.on('message', sendToSerial); // when a client sends a message,
+
+ client.on('close', function() { // when a client closes its connection
+ console.log("connection closed"); // print it out
+ var position = connections.indexOf(client); // get the client's position in the array
+ connections.splice(position, 1); // and delete it from the array
+ });
+}
+
+function sendToSerial(data) {
+ console.log("sending to serial: " + data);
+ myPort.write(data);
+}
+
+// This function broadcasts messages to all webSocket clients
+function broadcast(data) {
+ for (myConnection in connections) {   // iterate over the array of connections
+  connections[myConnection].send(data); // send the data to each connection
+ }
+}
+
+function sendSerialData(data) {
+   console.log(data);
+   // if there are webSocket connections, send the serial data
+   // to all of them:
+   if (connections.length > 0) {
+     broadcast(data);
+   }
+}
