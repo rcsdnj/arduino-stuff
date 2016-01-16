8cdc090478ce2ab400e9a458ac6d03c885c27981
Roberto Cássio Jr <roberto@escribo.com.br>
2016-01-15 22:21:55 -0300

    adding simple http server to show temperature on page

--- thermo-server/index.js
+++ thermo-server/index.js
@@ -66,3 +66,17 @@ function sendSerialData(data) {
      broadcast(data);
    }
 }
+
+var http = require('http');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./public");
+
+var server = http.createServer(function(req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+server.listen(8080);
